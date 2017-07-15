2ce458c670f773c6568fc791af98e4dd2cdd51d7
nowayingswang <nowayings@gmail.com>
2017-07-15 15:16:56 +0800

    填写相关接口

--- src/main/test/java/com/smart/CustomerServiceTest.java
+++ src/main/test/java/com/smart/CustomerServiceTest.java
@@ -2,10 +2,14 @@ package com.smart;
 
 import com.smart.model.Customer;
 import com.smart.service.CustomerService;
+import com.sun.tools.jdi.LinkedHashMap;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 测试类
@@ -27,5 +31,39 @@ public class CustomerServiceTest {
     @Test
     public void getCustomerListTest(){
         List<Customer> customerList = customerService.getCustomerList();
+        Assert.assertEquals(2,customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest(){
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest(){
+        Map<String,Object> fieldMap = new HashMap<String, Object>();
+        fieldMap.put("name","customer100");
+        fieldMap.put("contact","John");
+        fieldMap.put("telephone","2222222");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest(){
+        long id = 1;
+        Map<String,Object> fieldMap = new HashMap<>();
+        fieldMap.put("contact","Eric");
+        boolean result = customerService.updateCustomer(id,fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomerTest(){
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
     }
 }
